eed6e1752d3bfe2ac34bae57c2f81cea2fcabef3
aneeshpradhan2004 <aneeshpradhan2004@gmail.com>
2023-10-29 11:42:21 -0700

    Co-authored-by: MC <matthew03.carreon@gmail.com> Co-authored-by: BrandonSandovalS <BrandonSandovalS@users.noreply.github.com>

--- page/gt/index.js
+++ page/gt/index.js
@@ -1,14 +1,52 @@
+// Zepptchi's main page 
 import AutoGUI from '@silver-zepp/autogui';
 const gui = new AutoGUI();
 
 import { Step } from "@zos/sensor";
+
+// This variable will track our steps.
 const step = new Step();
 
-var my_text = gui.text("0");
-gui.render();
+class IndexPage {
+  init(){
+    const txt_steps = gui.text("Steps: 0");
 
-const changeCallback = () => {
-  my_text.update({ text: "Steps:" + step.getCurrent() });
-};
+    gui.newRow();
 
-step.onChange(changeCallback)
+    // Render the GUI.
+    gui.render();
+
+    // Callbacks.
+    const cb_Steps = () => {
+      txt_steps.update({ text: "Steps: " + step.getCurrent() });
+    };
+
+    // and on change events
+    step.onChange(cb_Steps)
+  }
+}
+
+Page({
+  build(){
+    this.indexPage = new IndexPage();
+    this.indexPage.init();
+  },
+  onDestroy(){
+    this.indexPage.destroy();
+  }
+})
+
+
+/** HELPERS (Credit to Silver) */
+
+// don't turn off the screen for 600 seconds
+import { setPageBrightTime } from '@zos/display'
+const result = setPageBrightTime({
+  brightTime: 600 * 1000,
+})
+
+// don't turn off the screen on wrist down for 600 seconds
+import { pauseDropWristScreenOff } from '@zos/display'
+pauseDropWristScreenOff({
+  duration: 600 * 1000,
+})
